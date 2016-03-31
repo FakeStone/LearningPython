15bd0d45920a608cea909e57bc8669c94e4c70f7
FakeStone <fakestonelee@gmail.com>
2016-03-31 23:56:21 +0800

    添加第一个学习文件和ignorefile

---
+++ IO.py
@@ -0,0 +1,15 @@
+#-*- coding=utf-8 -*_
+#输出使用[print] 多个语句可以用 逗号连接“,” 遇到逗号输出空格
+print "To live as a monster","or die as a good man"
+
+
+#输入使用raw_input 从终端获取[字符串]
+name = raw_input("please input your name:")
+print name
+
+#退出使用exit()
+exit(0)
+
+#变量的使用
+#|-变量存储[数据]的
+
